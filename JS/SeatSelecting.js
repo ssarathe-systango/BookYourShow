d7fa5358fb7125227254d36ec1921755731dfbee
sbiswas-systango <sbiswas@isystango.com>
2022-04-07 19:28:27 +0530

    number of seat selection

---
+++ JS/SeatSelecting.js
@@ -0,0 +1,52 @@
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+})
+
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+
+
+
+function selected_seats(id) {
+
+    let seat = document.getElementById(id).innerText;
+    window.localStorage.setItem('display1', seat);
+    
+}
+
+function submit_seats(){
+    
+    let value = window.localStorage.getItem('display1');
+    
+    console.log(value);
+}
