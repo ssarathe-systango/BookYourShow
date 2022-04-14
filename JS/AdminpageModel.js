2fd5fbb8843931b577c911d4b1bbe0ffc5aff302
sbiswas-systango <sbiswas@isystango.com>
2022-04-14 19:35:27 +0530

    dynamic admin page creation

---
+++ JS/AdminpageModel.js
@@ -0,0 +1,31 @@
+var modalWrap = null;
+const addMovie =(title, description)=>{
+    if(modalWrap !== null){
+            modalWrap.remove();
+
+    }
+    modalWrap = document.createElement('div');
+    modalWrap.innerHTML = `
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">${title}</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <p>${description}</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+  document.body.append(modalWrap);
+
+  var modal = new bootstrap.Modal(modalWrap.querySelector('.modal'));
+  modal.show();
+}
